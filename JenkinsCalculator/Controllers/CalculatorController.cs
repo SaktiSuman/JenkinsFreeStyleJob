1e677f5d7f5ba0c5c31d5fd87f95120989fc8222
Sakti Suman Tripathy <65999640+SaktiSuman@users.noreply.github.com>
2024-04-01 10:16:07 +0530

    Add files via upload

---
+++ JenkinsCalculator/Controllers/CalculatorController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JenkinsCalculator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("add")]
+        public int Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+        [HttpGet("subtract")]
+        public int Sub(int num1, int num2)
+        {
+            int result = num1 - num2;
+            return result;
+        }
+        [HttpGet("multiply")]
+        public int Multiply(int num1, int num2)
+        {
+            int result = num1 * num2;
+            return result;
+        }
+        [HttpGet("divide")]
+        public IActionResult Divide(int num1, int num2)
+        {
+            if(num2 == 0)
+            {
+                return BadRequest("Cannot divide by 0.");
+            }
+            int result = num1 / num2;
+            return Ok(result);
+        }
+    }
+}
